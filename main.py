730303958ce8e42f4a2a1529332f9c49663e50a1
Mikee0102 <mv236648@gmail.com>
2024-07-11 14:33:06 -0600

    Comit inicial

---
+++ main.py
@@ -0,0 +1,76 @@
+import psycopg2
+from flask import Flask, redirect, render_template
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import PasswordField, StringField, SubmitField
+
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+@app.route('/libros')
+def libros():
+    # Conectar con la base de datos
+    conexion = psycopg2.connect(
+        database="biblioteca3a",
+        user="postgres",
+        password="Kokies13",
+        host="localhost",
+        port="5432"
+    )
+    # crear un cursor (objeto para recorrer las tablas)
+    cursor = conexion.cursor()
+    # ejecutar una consulta en postgres
+    cursor.execute('''SELECT * FROM libros_view''')
+    #recuperar la informacion
+    datos = cursor.fetchall()
+    #cerrar cursos y conexion a la base de datos
+    cursor.close()
+    conexion.close()
+    return render_template('libros.html', datos=datos)
+
+@app.route('/autores')
+def autores():
+    # Conectar con la base de datos
+    conexion = psycopg2.connect(
+        database="biblioteca3a",
+        user="postgres",
+        password="Kokies13",
+        host="localhost",
+        port="5432"
+    )
+    # crear un cursor (objeto para recorrer las tablas)
+    cursor = conexion.cursor()
+    # ejecutar una consulta en postgres
+    cursor.execute('''SELECT * FROM autores_view''')
+    #recuperar la informacion
+    datos = cursor.fetchall()
+    #cerrar cursos y conexion a la base de datos
+    cursor.close()
+    conexion.close()
+    return render_template('autores.html', datos=datos)
+
+@app.route('/pais')
+def pais():
+    # Conectar con la base de datos
+    conexion = psycopg2.connect(
+        database="biblioteca3a",
+        user="postgres",
+        password="Kokies13",
+        host="localhost",
+        port="5432"
+    )
+    # crear un cursor (objeto para recorrer las tablas)
+    cursor = conexion.cursor()
+    # ejecutar una consulta en postgres
+    cursor.execute('''SELECT * FROM pais''')
+    #recuperar la informacion
+    datos = cursor.fetchall()
+    #cerrar cursos y conexion a la base de datos
+    cursor.close()
+    conexion.close()
+    return render_template('paises.html', datos=datos)
